73571b66e214294231b3677c8d0ea6c0f8292561
ciscucordo <fcordomi@gmail.com>
2015-10-10 17:19:28 +0200

    a

--- server.js
+++ server.js
@@ -47,9 +47,10 @@ var SampleApp = function() {
         };
 		
 		self.db = mysql.createConnection({
-			host: self.ipaddress,
-			user: "escacsvdt",
-			password: "escacsvdt",
+			host: process.env.OPENSHIFT_MYSQL_DB_HOST,
+			user: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+			password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+			port: process.env.OPENSHIFT_MYSQL_DB_PORT,
 			database: "escacsvdt"
 		});
 		
