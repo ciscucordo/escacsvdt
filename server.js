ceab5d2ba1939557610d69eff0d26195a2d23f7f
ciscucordo <fcordomi@gmail.com>
2015-10-12 21:02:48 +0200

    a

--- server.js
+++ server.js
@@ -252,7 +252,7 @@ var SampleApp = function() {
                     filePath = "public/escacsvdt/index.html";
 					
 					
-				//prova!!!
+				//////////////prova///////////////////!!!
 				} else if (pRequest.url == "/mysql") {
 					var paramsInSql = [];
 					self.db.query(
